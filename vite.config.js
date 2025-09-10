1000d709367e15dea07bbfc59812f0e4b2ef41a6
Adam C <adam.chamberlain910@gmail.com>
2025-09-10 21:23:18 +1000

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  root: './frontend',  // Tell Vite to use frontend as root
-  build: {
-    outDir: '../dist',  // Output to root dist folder
-    emptyOutDir: true
-  }
+  plugins: [react()]
 })
